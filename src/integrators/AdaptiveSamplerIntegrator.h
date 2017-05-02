1f5c12ee17b21cf8b3d52da2d1c7f08d10255019
Thomas Seidelmann <Thomas.Seidelmann@st.ovgu.de>
2017-05-03 00:57:59 +0200

    Added base class for the adaptive integrator. Currently functionality should be equal to the SamplerIntegrator. Refactored the methods to split them up in more manageable blocks. Added basic outline of needed functionality. The class is not yet integrated into pbrt - i.e. not accessible by the user and untested.

---
+++ src/integrators/AdaptiveSamplerIntegrator.h
@@ -0,0 +1,58 @@
+// core/integrator.h*
+#include "pbrt.h"
+#include "integrator.h"
+#include "primitive.h"
+#include "spectrum.h"
+#include "light.h"
+#include "reflection.h"
+#include "sampler.h"
+#include "material.h"
+
+namespace pbrt
+{
+
+class AdaptiveSamplerIntegrator : public Integrator
+{
+public:
+    // SamplerIntegrator Public Methods
+    AdaptiveSamplerIntegrator(std::shared_ptr<const Camera> camera,
+                              std::shared_ptr<Sampler> sampler,
+                              const Bounds2i &pixelBounds)
+                              : camera(camera), sampler(sampler), pixelBounds(pixelBounds) {}
+
+    virtual void Preprocess(const Scene &scene, Sampler &sampler) {}
+
+    void Render(const Scene &scene);
+
+    virtual Spectrum Li(const RayDifferential &ray, const Scene &scene,
+                        Sampler &sampler, MemoryArena &arena,
+                        int depth = 0) const = 0;
+
+    Spectrum SpecularReflect(const RayDifferential &ray,
+                             const SurfaceInteraction &isect,
+                             const Scene &scene, Sampler &sampler,
+                             MemoryArena &arena, int depth) const;
+
+    Spectrum SpecularTransmit(const RayDifferential &ray,
+                              const SurfaceInteraction &isect,
+                              const Scene &scene, Sampler &sampler,
+                              MemoryArena &arena, int depth) const;
+
+protected:
+    std::shared_ptr<const Camera> camera;
+
+    void CheckRadiance(Spectrum &radiance, const Point2i pixel, const std::unique_ptr<Sampler> &sampler);
+    Bounds2i BoundsForTile(const Point2i tile);
+    void ProcessTile(const Scene &scene, const Point2i tile);
+    void RenderTile(const Point2i tile);
+
+private:
+    std::shared_ptr<Sampler> sampler;
+    const Bounds2i pixelBounds;
+    Bounds2i sampleBounds;
+    Vector2i sampleExtent;
+    const int tileSize = 16;
+    Point2i nTiles;
+};
+
+}  // namespace pbrt
